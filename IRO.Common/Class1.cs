87d04e8597c660c089afffd6051ec732ae1334a2
jed.fletcher <jed@gratistech.com>
2020-04-01 16:28:11 -0400

    Added the common datetime interface for testing and custom timezones

--- IRO.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IRO.Common
-{
-    public class Class1
-    {
-    }
-}
---
+++ IRO.Common/IDateTime.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IRO.Common
+{
+    public interface IDateTime
+    {
+        DateTime Now { get; }
+    }
+}
